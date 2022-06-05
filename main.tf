78ccaf9ee9329f1ace157d27b284da96b9c0c1b4
Jon Carter <graphicnapkin>
2022-06-05 01:45:48 -0400

    cleaned up main.tf by removing old credentials import

--- main.tf
+++ main.tf
@@ -9,7 +9,6 @@ terraform {
 }
 
 provider "googleworkspace" {
-  #credentials = "/home/graphicnapkin/gcp/gws-terraform/gws-terraform-credentials.json"
   customer_id = "C034gw9n8"
   impersonated_user_email = "jc@graphicnapkin.com"
   oauth_scopes = ["https://www.googleapis.com/auth/admin.directory.orgunit"]
